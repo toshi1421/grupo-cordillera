5a17ef2e786a374686796aaaa65f87821ae7dbc0
Darlynee <darl.zamorano@duocuc.cl>
2026-05-08 00:37:08 -0400

    feat: limpieza de interfas

--- backend/ventas-service/src/main/java/com/cordillera/ventas/clients/InventarioClient.java
+++ backend/ventas-service/src/main/java/com/cordillera/ventas/clients/InventarioClient.java
@@ -4,11 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
-
-@FeignClient(name = "inventario-service", url = "http://localhost:8082/api/productos")
+@FeignClient(name = "inventario-service")
 public interface InventarioClient {
+    @GetMapping("/api/productos/verificar-stock")
+    boolean verificarStock(@RequestParam Long id, @RequestParam Integer cantidad);
 
-    @GetMapping("/verificar-stock")
-    boolean verificarStock(@RequestParam("id") Long id, @RequestParam("cantidad") Integer cantidad);
+    @PutMapping("/api/productos/{id}/descontar")
+    void descontarStock(@PathVariable Long id, @RequestParam Integer cantidad);
 }
