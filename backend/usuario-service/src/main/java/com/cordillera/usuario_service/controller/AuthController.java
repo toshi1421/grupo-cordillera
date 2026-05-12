3b050325771ecff7fa4d4e8e517b0f476e4fb4af
Darlynee <darl.zamorano@duocuc.cl>
2026-05-12 01:40:45 -0400

    feat: modificacion de las rutas

--- backend/usuario-service/src/main/java/com/cordillera/usuario_service/controller/AuthController.java
+++ backend/usuario-service/src/main/java/com/cordillera/usuario_service/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/usuarios/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final UsuarioService service;
